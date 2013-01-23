c61e7fccce72479599582942c5f6d752514d44cc
Jon Rowe <hello@jonrowe.co.uk>
2013-01-23 20:51:32 +1100

    bundler bootstrap

---
+++ lib/rack-nullsession.rb
@@ -0,0 +1,7 @@
+require "rack-nullsession/version"
+
+module Rack
+  module Nullsession
+    # Your code goes here...
+  end
+end
---
+++ lib/rack-nullsession/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module Nullsession
+    VERSION = "0.0.1"
+  end
+end
